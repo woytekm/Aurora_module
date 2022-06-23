ddb8d1a27b174adcdf4cf027b153a625814b4e5f
Wojtek Mitus <woytekm@gmail.com>
2022-06-24 01:08:54 +0200

    convert any number of GPX to single GeoJSON object. experiment with similar vector detection.

--- applications/BLS2-rear/pothole/openlayers/gpx_to_geojson.py
+++ applications/BLS2-rear/pothole/openlayers/gpx_to_geojson.py
@@ -1,31 +1,52 @@
 #!/opt/local/bin/python3.7
-
 import pandas as pd
 from gpxcsv import gpxtolist
 from geojson import Feature, FeatureCollection, Point, LineString
+import shapely.geometry
 import json
 import sys
+import os
+import re 
 
-
-df1 = pd.DataFrame(gpxtolist(sys.argv[1]))
-df2 = pd.DataFrame(gpxtolist(sys.argv[2]))
-
+import_path= 'gpx_import'
 features = []
+overlapping_features = []
+index = 0
 
-# columns used for constructing geojson object
-#features = df.apply(lambda row: Feature(geometry=Point((float(row['lon']), float(row['lat'])), properties={"shock":row['shock'],"ele":row['ele'],"time":row['time']})),axis=1).tolist()
+for filename in os.listdir(import_path):
+    infile = os.path.join(import_path,filename)
+    if os.path.isfile(infile):
+     if(re.match('.*\.gpx$',infile)):
+       print("adding "+infile)
+       df = pd.DataFrame(gpxtolist(infile))
+       for row in df.index:
+        if(row >= df.index.size-1):
+          break
+        f = Feature(geometry=LineString([(df['lon'][row], df['lat'][row]), (df['lon'][row+1], df['lat'][row+1])]), properties={"shock":df['shock'][row+1],"ele":df['ele'][row+1],"time":df['time'][row+1]})
 
-for df in df1,df2:
- for row in df.index:
-  if(row >= df.index.size-1):
-   break
-  f = Feature(geometry=LineString([(df['lon'][row], df['lat'][row]), (df['lon'][row+1], df['lat'][row+1])]), properties={"shock":df['shock'][row+1],"ele":df['ele'][row+1],"time":df['time'][row+1]})
-  features.append(f)
+        # Does not work. It detects only identical vectors, not similar ones.
+        #
+        #ls1 = shapely.geometry.LineString([(f.geometry.coordinates[0][0],f.geometry.coordinates[0][1]),(f.geometry.coordinates[1][0],f.geometry.coordinates[1][1])])
+        #for test_feat in features:
+        #  ls2 = shapely.geometry.LineString([(test_feat.geometry.coordinates[0][0],test_feat.geometry.coordinates[0][1]),(test_feat.geometry.coordinates[1][0],test_feat.geometry.coordinates[1][1])])
+        #  if(ls1.almost_equals(ls2,decimal=9)):
+        #    print("vector overlap detected!")
+        #    overlapping_features.append(f)         
 
-# whole geojson object
+        # A bit better one (Hausdorff distance)::
+        # ls1 = shapely.geometry.LineString([(f.geometry.coordinates[0][0],f.geometry.coordinates[0][1]),(f.geometry.coordinates[1][0],f.geometry.coordinates[1][1])])
+        # for test_feat in features:
+        #   ls2 = shapely.geometry.LineString([(test_feat.geometry.coordinates[0][0],test_feat.geometry.coordinates[0][1]),(test_feat.geometry.coordinates[1][0],test_feat.geometry.coordinates[1][1])])
+        #   hd = ls1.hausdorff_distance(ls2)
+        #   if(hd<0.000089):                 # need to tweak this value
+        #     print("vector overlap detected! hd:"+str(hd))
+        #     overlapping_features.append(f)
+
+        features.append(f)
+        index += 1
+        print(">> "+str(index)+" <<", end="\r")
+
 feature_collection = FeatureCollection(features=features)
-
-out_fname = sys.argv[1].split(".")[0]
 
 with open("combined.json", 'w', encoding='utf-8') as f:
     json.dump(feature_collection, f, ensure_ascii=False, indent=4)
