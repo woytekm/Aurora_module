1b8c2ed2375ef890c014797503c031b8215d08cd
Wojtek Mitus <woytekm@gmail.com>
2023-01-08 21:28:10 +0100

    add track caching feature to furhter speed up conversion

--- applications/BLS2-rear/pothole/openlayers/gpx_to_geojson.py
+++ applications/BLS2-rear/pothole/openlayers/gpx_to_geojson.py
@@ -9,6 +9,49 @@ import json
 import sys
 import os
 import re
+import uuid
+import np
+import hausdorff
+
+
+def isparrallel(ls1, ls2):
+    xs1, xe1 = ls1.xy[0]
+    ys1, ye1 = ls1.xy[1]
+    xs2, xe2 = ls2.xy[0]
+    ys2, ye2 = ls2.xy[1]
+
+    if xs1 == xe1:
+        angle1 = np.pi/2
+    else:
+        angle1 = np.arctan((ye1-ys1)/(xe1-xs1))
+    if xs2 == xe2:
+        angle2 = np.pi/2
+    else:
+        angle2 = np.arctan((ye2-ys2)/(xe2-xs2))
+
+    return True if angle1 == angle2 else False
+
+
+def cache_track(track,dir):
+ if not os.path.exists(dir):
+   try:
+     os.makedirs(dir)
+   except:
+     print("Cannot create track cache directory.")
+     sys.exit()
+
+ with open(dir+"/"+str(track.uuid)+".json", 'w', encoding='utf-8') as f:
+   json.dump(track.features, f, ensure_ascii=False, indent=4)
+
+
+
+def load_cached_track(infile):
+ nt = Track(-1)
+ print("loading cached "+infile+"...")
+ with open(infile, 'r', encoding='utf-8') as f:
+   nt.populate_from_json(json.load(f))
+ nt.update_bbox()
+ return nt
 
 
 def geom_remove(geojson_features, geom):
@@ -16,8 +59,8 @@ def geom_remove(geojson_features, geom):
    linestrings_to_remove = []
    prev_coord = (0,0)
 
-   print("removing geometry: ", end="")
-   print(geom)
+#   print("removing geometry: ", end="")
+#   print(geom)
 
    for coord in geom.coords:
      if (prev_coord[0] == 0) and (prev_coord[1] == 0):
@@ -32,7 +75,7 @@ def geom_remove(geojson_features, geom):
           (ls[1] == feat.geometry.coordinates[0][1]) and
           (ls[2] == feat.geometry.coordinates[1][0]) and
           (ls[3] == feat.geometry.coordinates[1][1]) ):
-         print("removed vector:"+str(feat.geometry.coordinates[0][0])+","+str(feat.geometry.coordinates[0][1])+","+str(feat.geometry.coordinates[1][0])+","+str(feat.geometry.coordinates[1][1]))
+ #        print("removed vector:"+str(feat.geometry.coordinates[0][0])+","+str(feat.geometry.coordinates[0][1])+","+str(feat.geometry.coordinates[1][0])+","+str(feat.geometry.coordinates[1][1]))
          geojson_features.remove(feat)
 
 
@@ -46,11 +89,20 @@ class Track:
     bbox_max_lat = 0
     overlaps = []
     features = []
+    uuid = ""
 
     def __init__(self,idn):
        self.idnum = idn
        self.features = []
        self.overlaps = []
+       self.uuid = ""
+
+    def populate_from_json(self,json_array):
+       for row in json_array:
+         f = Feature(geometry=LineString([(row['geometry']['coordinates'][0][0], row['geometry']['coordinates'][0][1]),(row['geometry']['coordinates'][1][0],row['geometry']['coordinates'][1][1])]), properties={"shock":row['properties']['shock'],"ele":row['properties']['ele'],"time":row['properties']['time']})
+         self.features.append(f)
+       self.update_bbox()
+
 
     def populate_from_gpxlist(self,trackpoints):
        self.dframe = pd.DataFrame(trackpoints)
@@ -116,20 +168,46 @@ index = 0
 
 ids = 0
 
+create_cache = True
+use_cache = True
+cache_path = "gpx_to_geojson.cache"
+
+# load already processed, cached geojson features
+if use_cache == True:
+ for filename in os.listdir(cache_path):
+    infile = os.path.join(cache_path,filename)
+    if os.path.isfile(infile):
+      nct = load_cached_track(infile)
+      tracks.append(nct)
+
+# build output data from set of provided gpx files
+
 for filename in os.listdir(import_path):
     infile = os.path.join(import_path,filename)
     if os.path.isfile(infile):
      if(re.match('.*\.gpx$',infile,re.IGNORECASE)):
 
        print("adding "+infile)
+       stat_data = os.stat(infile)
+       file_len = stat_data.st_size
+
        ids += 1
        nt = Track(ids)
+       # unique track identifier UUID is derived from file name + file length. TODO: add md5 sum from first 1KB of the file to better identify it.
+       nt.uuid = str(uuid.uuid5(uuid.NAMESPACE_DNS, str(file_len)+infile))
+       if use_cache == True:
+         if os.path.isfile(cache_path+"/"+nt.uuid+'.json'):
+           print("skipping "+infile+" - already in cache")
+           del nt
+           continue
+  
        nt.populate_from_gpxlist(gpxtolist(infile))
        print(str(len(nt.features))+' vectors')
 
-       if(ids == 1):
-         tracks.append(nt)
-         continue
+       if use_cache == False:
+         if(ids == 1):
+           tracks.append(nt)
+           continue
 
        for track in tracks:
         if(track.idnum != nt.idnum):
@@ -154,7 +232,10 @@ for filename in os.listdir(import_path):
                  ls2 = shapely.geometry.LineString([(test_feat.geometry.coordinates[0][0],test_feat.geometry.coordinates[0][1]),(test_feat.geometry.coordinates[1][0],test_feat.geometry.coordinates[1][1])])
                  hd = ls1.hausdorff_distance(ls2)
                  if(hd<hausdorff_distance_trigger):
-                   print("vector overlap detected! hd:"+str(hd))
+                   shockA = f.properties['shock']
+                   shockB = test_feat.properties['shock']
+                   print("vector overlap detected! hd:"+str(hd)+" shock A:"+str(shockA)+", shock B:"+str(shockB)+", avg shock:"+str((shockA+shockB)/2))
+                       
                    geom_remove(nt.features,ls1)
         
             for vrfied_feat in nt.features:
@@ -176,6 +257,9 @@ for filename in os.listdir(import_path):
 
 for track in tracks:
   all_features.extend(track.features)
+  if((create_cache == True) and (track.idnum != -1) ):  # -1 means that this is already cached track loaded from cache
+    if not os.path.isfile(cache_path+"/"+track.uuid+'.json'):
+      cache_track(track,cache_path)
 
 feature_collection = FeatureCollection(features=all_features)
 
